d3b54cddce6ebc75d66faf6407d7b450cfbab180
Osama Najjar <osama.najjar@bizzomate.com>
2018-07-11 14:52:29 +0200

    added assets folder which will have the required files for a pwa

--- gulpfile.js
+++ gulpfile.js
@@ -10,21 +10,40 @@ if (config) {
     "deploymentFolderPath",
     "extentionsToBeCached"
   ];
-  requiredConfProps.forEach(prop=>{
-    if(!config[prop]){
-        throw new ReferenceError(
-            `'${prop}' property is required`,
-            "bizzo.config.json"
-          );
+  requiredConfProps.forEach(prop => {
+    if (!config[prop]) {
+      throw new ReferenceError(
+        `'${prop}' property is required`,
+        "bizzo.config.json"
+      );
     }
   });
 }
 
+gulp.task("generate-sw", () => {
+  swPreChache.write(`${config.sourceFolderPath}/bizzo-sw.js`, {
+    staticFileGlobs: [
+      `${
+        config.deploymentFolderPath
+      }/**/*.{${config.extentionsToBeCached.join()}}`,
+      `${config.deploymentFolderPath}/index.html`,
+      `${config.deploymentFolderPath}/login.html`
+    ],
+    stripPrefix: `${config.deploymentFolderPath}`,
+    navigateFallback: "/"
+  });
+});
 
-gulp.task('generate-sw', function() {
-    swPrecache.write(`${config.sourceFolderPath}/bizzo-sw.js`, {
-      staticFileGlobs: [ `${config.deploymentFolderPath}/**/*.{${config.extentionsToBeCached.join()}}`,`${config.deploymentFolderPath}/index.html`,`${config.deploymentFolderPath}/login.html`],
-      stripPrefix: `${config.sourceFolderPath}`,
-      navigateFallback: '/index.html'
-    });
+gulp.task("watch:sw", () => {
+  const exts = config.extentionsToBeCached.map(
+    ext => `${config.deploymentFolderPath}/**/*.${ext}`
+  );
+  const watcher = gulp.watch(exts, ["generate-sw"]);
+  watcher.on("change", event => {
+    console.log(`[Captured Change in ${event.path} : ${event.type}`);
+  });
+});
+
+gulp.task("write:index.html",()=>{
+    gulp.src("./assets/index.html").pipe(gulp.dest("./theme/",{overwrite:true}));
 });

197506cefdae9af632586a36f146075f2c1cb67d
Osama Najjar <osama.najjar@bizzomate.com>
2018-07-12 17:07:44 +0200

    added set bizzo tags & scripts injects

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 // importing bizzo config object.
 const config = require("./bizzo.config.json");
 const gulp = require("gulp");
-const gulpCopy = require('gulp-copy');
+const gulpCopy = require("gulp-copy");
+const inject = require("gulp-inject");
 const swPreChache = require("sw-precache");
 
 // handle required config props
@@ -9,7 +10,8 @@ if (config) {
   const requiredConfProps = [
     "sourceFolderPath",
     "deploymentFolderPath",
-    "extentionsToBeCached"
+    "extentionsToBeCached",
+    "themeColor"
   ];
   requiredConfProps.forEach(prop => {
     if (!config[prop]) {
@@ -21,7 +23,7 @@ if (config) {
   });
 }
 
-gulp.task("generate-sw", () => {
+gulp.task("write:sw", () => {
   swPreChache.write(`${config.sourceFolderPath}/bizzo-sw.js`, {
     staticFileGlobs: [
       `${
@@ -45,25 +47,61 @@ gulp.task("watch:sw", () => {
   });
 });
 
-gulp.task("write:index.html",()=>{
-    gulp.src("./assets/index.html")
-        .pipe(gulpCopy(`${config.sourceFolderPath}/`,{prefix:1}));  
+gulp.task("write:manifest.json", () => {
+  gulp
+    .src("./assets/manifest.json")
+    .pipe(gulpCopy(`${config.sourceFolderPath}/`, { prefix: 1 }));
 });
-gulp.task("write:manifest.json",()=>{
-    gulp.src("./assets/manifest.json")
-        .pipe(gulpCopy(`${config.sourceFolderPath}/`,{prefix:1}));
-});
-gulp.task("write:icons",()=>{
-    gulp.src("./assets/icons/*.png")
-        .pipe(gulpCopy(`${config.sourceFolderPath}/`,{prefix:1}));
-});
-gulp.task("write:icons",()=>{
-  gulp.src("./assets/icons/*.png")
-      .pipe(gulpCopy(`${config.sourceFolderPath}/`,{prefix:1}));
+gulp.task("write:icons", () => {
+  gulp
+    .src("./assets/pwa_icons/*.png")
+    .pipe(gulpCopy(`${config.sourceFolderPath}/`, { prefix: 1 }));
 });
 
-gulp.task("write:bizzo-sw-register",()=>{
-  gulp.src("./assets/bizzo-sw-register.js")
-      .pipe(gulpCopy(`${config.sourceFolderPath}/`,{prefix:1}));
+gulp.task("write:bizzo-sw-register", () => {
+  gulp
+    .src("./assets/bizzo-sw-register.js")
+    .pipe(gulpCopy(`${config.sourceFolderPath}/`, { prefix: 1 }));
 });
 
+gulp.task("inject:tags", () => {
+  gulp
+    .src(`${config.sourceFolderPath}/index.html`)
+    .pipe(
+      inject(gulp.src("./assets/_bizzo-tags.html"), {
+        name: "bizzo:tags",
+        starttag: "<!-- bizzo:tags -->",
+        transform: (path, file) => {
+          let content = file.contents.toString("utf8");
+          content = content.replace("#$theme#", config.themeColor);
+          return content;
+        }
+      })
+    )
+    .pipe(gulp.dest(`${config.sourceFolderPath}/`));
+});
+
+gulp.task("inject:scripts", () => {
+  gulp
+    .src(`${config.sourceFolderPath}/index.html`)
+    .pipe(
+      inject(gulp.src("./assets/_bizzo-scripts.html"), {
+        name: "bizzo:scripts",
+        starttag: "<!-- bizzo:scripts -->",
+        transform: (path, file) => {
+          let content = file.contents.toString("utf8");
+          return content;
+        }
+      })
+    )
+    .pipe(gulp.dest(`${config.sourceFolderPath}/`));
+});
+
+gulp.task("mx-pwa", [
+  "write:sw",
+  "write:manifest.json",
+  "write:icons",
+  "write:bizzo-sw-register",
+  "inject:tags",
+  "inject:scripts"
+]);

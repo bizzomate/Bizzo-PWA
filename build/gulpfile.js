9daaee27c0f4f7d85673486b1b4ccd78f23798c3
Osama Najjar <osama.najjar@bizzomate.com>
2018-07-24 14:44:38 +0200

    added gulp-zip to create, bizzo-pwa package

---
+++ build/gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const zip = require('gulp-zip');
+ 
+gulp.task('create-pwa-package', () =>
+    gulp.src([
+        '../assets',
+        '../package.json',
+        '../bizzo.config.json',
+        '../gulpfile.js'
+    ])
+        .pipe(zip('bizzo-pwa.zip'))
+        .pipe(gulp.dest('dist'))
+);
